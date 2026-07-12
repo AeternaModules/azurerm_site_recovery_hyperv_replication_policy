5bd9a1ec3b61df92d540fd72acb0a8d9d68c1886
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:26 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "site_recovery_hyperv_replication_policies" {
-  description = "All site_recovery_hyperv_replication_policy resources"
-  value       = azurerm_site_recovery_hyperv_replication_policy.site_recovery_hyperv_replication_policies
-}
 output "site_recovery_hyperv_replication_policies_application_consistent_snapshot_frequency_in_hours" {
-  description = "List of application_consistent_snapshot_frequency_in_hours values across all site_recovery_hyperv_replication_policies"
-  value       = [for k, v in azurerm_site_recovery_hyperv_replication_policy.site_recovery_hyperv_replication_policies : v.application_consistent_snapshot_frequency_in_hours]
+  description = "Map of application_consistent_snapshot_frequency_in_hours values across all site_recovery_hyperv_replication_policies, keyed the same as var.site_recovery_hyperv_replication_policies"
+  value       = { for k, v in azurerm_site_recovery_hyperv_replication_policy.site_recovery_hyperv_replication_policies : k => v.application_consistent_snapshot_frequency_in_hours }
 }
 output "site_recovery_hyperv_replication_policies_name" {
-  description = "List of name values across all site_recovery_hyperv_replication_policies"
-  value       = [for k, v in azurerm_site_recovery_hyperv_replication_policy.site_recovery_hyperv_replication_policies : v.name]
+  description = "Map of name values across all site_recovery_hyperv_replication_policies, keyed the same as var.site_recovery_hyperv_replication_policies"
+  value       = { for k, v in azurerm_site_recovery_hyperv_replication_policy.site_recovery_hyperv_replication_policies : k => v.name }
 }
 output "site_recovery_hyperv_replication_policies_recovery_point_retention_in_hours" {
-  description = "List of recovery_point_retention_in_hours values across all site_recovery_hyperv_replication_policies"
-  value       = [for k, v in azurerm_site_recovery_hyperv_replication_policy.site_recovery_hyperv_replication_policies : v.recovery_point_retention_in_hours]
+  description = "Map of recovery_point_retention_in_hours values across all site_recovery_hyperv_replication_policies, keyed the same as var.site_recovery_hyperv_replication_policies"
+  value       = { for k, v in azurerm_site_recovery_hyperv_replication_policy.site_recovery_hyperv_replication_policies : k => v.recovery_point_retention_in_hours }
 }
 output "site_recovery_hyperv_replication_policies_recovery_vault_id" {
-  description = "List of recovery_vault_id values across all site_recovery_hyperv_replication_policies"
-  value       = [for k, v in azurerm_site_recovery_hyperv_replication_policy.site_recovery_hyperv_replication_policies : v.recovery_vault_id]
+  description = "Map of recovery_vault_id values across all site_recovery_hyperv_replication_policies, keyed the same as var.site_recovery_hyperv_replication_policies"
+  value       = { for k, v in azurerm_site_recovery_hyperv_replication_policy.site_recovery_hyperv_replication_policies : k => v.recovery_vault_id }
 }
 output "site_recovery_hyperv_replication_policies_replication_interval_in_seconds" {
-  description = "List of replication_interval_in_seconds values across all site_recovery_hyperv_replication_policies"
-  value       = [for k, v in azurerm_site_recovery_hyperv_replication_policy.site_recovery_hyperv_replication_policies : v.replication_interval_in_seconds]
+  description = "Map of replication_interval_in_seconds values across all site_recovery_hyperv_replication_policies, keyed the same as var.site_recovery_hyperv_replication_policies"
+  value       = { for k, v in azurerm_site_recovery_hyperv_replication_policy.site_recovery_hyperv_replication_policies : k => v.replication_interval_in_seconds }
 }
 
